0dbff2258d34c5b080061edc752db3d4fac99f27
TFX0019 <1400kss@gmail.com>
2022-08-12 02:43:45 -0400

    updated

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,5 +1,5 @@
 // export const baseUri = "http://localhost:4001";
-export const baseUri = 'https://apimanda2.herokuapp.com/';
+export const baseUri = 'https://apimanda2.herokuapp.com';
 export const prefixUri = "/api/v1";
 export const TOKEN = "token";
 export const USER_DATA = "user@data";
