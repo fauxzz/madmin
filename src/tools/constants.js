bda47e9fb26c3f269ddec6a5c574286fbd0bb46c
TFX0019 <1400kss@gmail.com>
2022-08-19 09:51:38 -0400

    change api uri

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,5 +1,5 @@
 // export const baseUri = "http://localhost:4001";
-export const baseUri = 'https://apimanda2.herokuapp.com';
+export const baseUri = 'https://api.manda2.ga';
 export const prefixUri = "/api/v1";
 export const TOKEN = "token";
 export const USER_DATA = "user@data";
